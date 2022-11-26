e7455efcdbc8e1c4ee9ced52ced0e6ca42e78d5c
David Sabine <DavidSabine@users.noreply.github.com>
2022-11-25 19:24:05 -0500

    demonstration-of-bug

---
+++ __tests__/weird.test.mjs
@@ -0,0 +1,18 @@
+import { expect, test, vi } from 'vitest'
+import { join as joinPath } from 'node:path'
+
+const helloWorld = () => {
+  return joinPath('hello', 'world')
+}
+
+test('Are you mocking me?', () => {
+  // note all the indents in this file are tabs
+  // change the indentation line 12 from tab to spaces
+  // test pass with spaces, test fails with tab
+	vi.mock('node:path', () => {
+    return {
+      join: vi.fn().mockReturnValue('goodbye world')
+    }
+  })
+  expect(helloWorld()).toBe('goodbye world')
+})
