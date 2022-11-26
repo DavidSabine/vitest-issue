ea872ae687460c855199a68f01358391162d3e99
David Sabine <DavidSabine@users.noreply.github.com>
2022-11-25 19:27:52 -0500

    remove all doubt

--- __tests__/weird.test.mjs
+++ __tests__/weird.test.mjs
@@ -2,17 +2,17 @@ import { expect, test, vi } from 'vitest'
 import { join as joinPath } from 'node:path'
 
 const helloWorld = () => {
-  return joinPath('hello', 'world')
+return joinPath('hello', 'world')
 }
 
 test('Are you mocking me?', () => {
-  // note all the indents in this file are tabs
-  // change the indentation line 12 from tab to spaces
-  // test pass with spaces, test fails with tab
+// note all the indents in this file are tabs
+// change the indentation line 12 from tab to spaces
+// test pass with spaces, test fails with tab
 	vi.mock('node:path', () => {
-    return {
-      join: vi.fn().mockReturnValue('goodbye world')
-    }
-  })
-  expect(helloWorld()).toBe('goodbye world')
+return {
+join: vi.fn().mockReturnValue('goodbye world')
+}
+})
+expect(helloWorld()).toBe('goodbye world')
 })
