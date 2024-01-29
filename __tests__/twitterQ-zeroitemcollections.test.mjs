1f637508b1da88c3e9c819823983138abe1eb26e
David Sabine <DavidSabine@users.noreply.github.com>
2024-01-29 12:35:07 -0500

    response to Milan's Q

--- __tests__/twitterQ-zeroitemcollections.test.mjs
+++ __tests__/twitterQ-zeroitemcollections.test.mjs
@@ -1,39 +1,34 @@
-import { describe, expect, test, vi } from 'vitest'
+import { describe, expect, test } from 'vitest'
 
 // Original Q at https://twitter.com/mlntdrv/status/1750935348429930817
 
-const cocoToFetch = (request = null, cargo = null) => {
+// When you test collections you start with 0 items, then 1. This forces you 
+// to write a conditional. Then you test for 2, so you now change the if to 
+// a loop.
+// But if you start with 1 and then 2, you end up at the loop again, but 
+// without the test for 0. Is this test needed then?
+
+const cocoToFetch = (request = null) => {
 	switch(request) {
-		case `cookie`: return []
+		case `mail`: return [Array.from({ length: Math.floor(Math.random) * 10}, () => ({}))]
 		case `shoes`: return [{}, {}]
 		case `toy`: return [{}]
-		case `mail`: return [`key`, {}]
 		default: return []
 	}
 }
 
-
-describe(`A collection please`, () => {
-	test('A collection please', () => {
+describe(`Coco's behaviours`, () => {
+	test(`Coco can fetch stuff`, () => {
 		expect(cocoToFetch()).toBeTypeOf(`object`)
 		expect(Array.isArray(cocoToFetch())).toBe(true)
 	})
-	test(`Coco never brings me cookies`, () => {
-		expect(cocoToFetch(`cookie`).length).toEqual(0)
-	})
-	test(`Coco brings 1 toy`, () => {
+	test(`Coco can fetch a toy`, () => {
 		expect(cocoToFetch(`toy`).length).toEqual(1)
 	})
-	test(`Coco brings me 2 shoes`, () => {
+	test(`Coco can fetch shoes`, () => {
 		expect(cocoToFetch(`shoes`).length).toEqual(2)
 	})
-	test(`Coco checks the mail`, () => {
-		expect(cocoToFetch(`mail`).length).toBeGreaterThanOrEqual(1)
+	test(`Coco can fetch the mail`, () => {
+		expect(cocoToFetch(`mail`).length).toBeGreaterThanOrEqual(0)
 	})
 })
-
-
-
-// type NonEmptyArray<T> = [T, ...T[]];
-// Example usage
-// let myNonEmptyArray: NonEmptyArray<number> = [1, 2, 3];
